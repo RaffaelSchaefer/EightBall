4af786dac1e2b5b961d1f89f94598446128bc8b4
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-04-06 09:53:29 +0200

    Added more Answers

--- eightBall.js
+++ eightBall.js
@@ -1,4 +1,25 @@
-var eightBallAnswers = ["It is certain","It is decidedly so","Without a doubt","Yes – definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Don’t count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+var eightBallAnswers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes – definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don’t count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+]
 function eightBall () {
     document.getElementById("Output").innerHTML = eightBallAnswers[Math.floor(Math.random() * eightBallAnswers.length)]
 }
